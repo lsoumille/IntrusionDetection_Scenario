62431dbb72da257cade5371993b5873568d65ed5
lsoumille <lucas.soumille@etu.unice.fr>
2017-10-30 11:26:25 +0100

    add ddos scenario

--- resources/injector/demo/ddos/launchIncident.sh
+++ resources/injector/demo/ddos/launchIncident.sh
@@ -2,10 +2,10 @@
 
 echo "Starting log injecton for DDOS ..."
 echo "Inject logs from PaloAlto ..."
-punchplatform-log-injector.sh -c palo_alto_firewall_allow.json > /dev/null &
+punchplatform-log-injector.sh -c palo_alto_firewall_allow.json > /dev/null & echo $!
 echo "Inject logs from Proxy ..."
-punchplatform-log-injector.sh -c squid.json > /dev/null &
+punchplatform-log-injector.sh -c squid.json > /dev/null & echo $!
 echo "Inject logs from Apache ..."
-punchplatform-log-injector.sh -c apache_httpd_injector.json > /dev/null &
+punchplatform-log-injector.sh -c apache_httpd_injector.json > /dev/null & echo $!
 echo "Injection done"
 
--- resources/injector/demo/ddos/launchNoise.sh
+++ resources/injector/demo/ddos/launchNoise.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "Starting Noise Injecton ..."
-punchplatform-log-injector.sh -c ../noise/apache/apache_httpd_injector.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/unix/psad_noise.json >/dev/null &
-punchplatform-log-injector.sh -c ../noise/unix/rkhunter_noise.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/auth/active_directory_noise.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/av/symantec_daily_scan_noise.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/firewalls/checkpoint/checkpoint_firewall_ingoing_noise.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/firewalls/checkpoint/checkpoint_firewall_outgoing_noise.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_firewall_noiseallow.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_firewall_noisedeny.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_firewall_outgoing_allow.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_outgoing_deny.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/ids/ids_noise.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/proxy/squid_noise.json > /dev/null &
-punchplatform-log-injector.sh -c ../noise/windows/device_management_noise.json > /dev/null &
+punchplatform-log-injector.sh -c ../noise/apache/apache_httpd_injector.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/unix/psad_noise.json >/dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/unix/rkhunter_noise.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/auth/active_directory_noise.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/av/symantec_daily_scan_noise.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/firewalls/checkpoint/checkpoint_firewall_ingoing_noise.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/firewalls/checkpoint/checkpoint_firewall_outgoing_noise.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_firewall_noiseallow.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_firewall_noisedeny.json > /dev/null & echo $! 
+punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_firewall_outgoing_allow.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/firewalls/paloalto/palo_alto_outgoing_deny.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/ids/ids_noise.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/proxy/squid_noise.json > /dev/null & echo $!
+punchplatform-log-injector.sh -c ../noise/windows/device_management_noise.json > /dev/null & echo $!
 echo "Injection done"
